25737c17f569bd9a22bee922ef56fc1b395fb846
bafab <bafa@tutanota.de>
2018-05-09 19:48:49 -0400

    Done.

--- src/components/TweetWall.js
+++ src/components/TweetWall.js
@@ -10,10 +10,26 @@ class TweetWall extends React.Component {
     };
   }
 
-  // TODO: componentWillMount()
-  // TODO: shouldComponentUpdate()
-  // TODO: componentWillReceiveProps()
+  componentWillMount() {
+    this.setState({
+      tweets: this.props.newTweets
+    })
+  }
 
+  shouldComponentUpdate(newProps){
+    return newProps.newTweets.length > 0
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      tweets: [
+        ...nextProps.newTweets,
+        ...this.state.tweets
+
+      ]
+    })
+  }
+  
   render() {
     const tweets = this.state.tweets.map((tweet, index) => <Tweet text={tweet.text} key={index} />);
 
